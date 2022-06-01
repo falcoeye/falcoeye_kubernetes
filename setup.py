f5d7835c707a6bd79ba1bbccd44c2a4f2435fb9e
Abdulelah Bin Mahfoodh <abduh.bm@gmail.com>
2022-06-01 18:11:58 +0300

    Update setup

--- setup.py
+++ setup.py
@@ -1,39 +1,76 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import os
+import sys
+from shutil import rmtree
 
-import setuptools
-from setuptools import find_packages
+from setuptools import Command, find_packages, setup
 
-DIR = os.path.dirname(os.path.realpath(__file__))
+NAME = "falcoeye_kubernetes"
+DESCRIPTION = "k8s config files for falcoeye"
+URL = "https://github.com/falcoeye/falcoeye_kubernetes"
+AUTHOR = "falcoeye Team"
+
+REQUIRED = ["kubernetes", "requests", "PyYAML"]
+
+here = os.path.abspath(os.path.dirname(__file__))
 
 with open("README.md", "r") as f:
     long_description = f.read()
 
-try:
-    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+about = {}
+with open(os.path.join(here, NAME, "__version__.py")) as f:
+    exec(f.read(), about)
 
-    class bdist_wheel(_bdist_wheel):
-        def finalize_options(self):
-            _bdist_wheel.finalize_options(self)
-            self.root_is_pure = False
 
-except ImportError:
-    print("Cannot import bdist_wheel, upgrade wheel package")
-    bdist_wheel = None
+class UploadCommand(Command):
+    """Support setup.py upload."""
 
-package = "falcoeye_kubernetes"
-packages = [f"{package}"] + [f"{package}.{p}" for p in find_packages(f"../{package}")]
+    description = "Build and publish the package."
+    user_options = []
 
-setuptools.setup(
-    name=f"{package}",
-    description="Falcoeye kubernetes library",
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
+        except OSError:
+            pass
+
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
+
+        self.status("Uploading the package to PyPi via Twine…")
+        os.system("twine upload dist/*")
+
+        sys.exit()
+
+
+setup(
+    name=NAME,
+    version=about["__version__"],
+    description=DESCRIPTION,
     long_description=long_description,
-    url="https://github.com/falcoeye/falcoeye_kubernetes",
-    packages=packages,
-    package_dir={package: f"{DIR}/."},
-    zip_safe=False,
-    data_files=[
-        ("", ["README.md"]),
+    author=AUTHOR,
+    url=URL,
+    packages=find_packages(exclude=["tests"]),
+    include_package_data=True,
+    install_requires=REQUIRED,
+    license="MIT",
+    keywords="falcoeye",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
     ],
+    # $ setup.py publish support.
+    cmdclass={"upload": UploadCommand},
 )
-wheel = os.path.realpath(DIR + "/dist")
-print(f"Python wheel saved: {wheel}")
