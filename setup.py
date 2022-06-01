4d13dfe58321a893d9a4d8ae1e163ea99531ea42
Abdulelah Bin Mahfoodh <abduh.bm@gmail.com>
2022-06-01 18:38:38 +0300

    Fix

--- setup.py
+++ setup.py
@@ -64,7 +64,9 @@ setup(
     author=AUTHOR,
     url=URL,
     packages=find_packages(exclude=["tests"]),
-    include_package_data=True,
+    package_data={
+        "": ["*.yaml", "*.yml"],
+    },
     install_requires=REQUIRED,
     license="MIT",
     keywords="falcoeye",
